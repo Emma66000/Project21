d8a58f1568054188ec15c7ab5c930b59dec0356f
Emma66000 <ingargio@etud.insa-toulouse.fr>
2021-11-05 16:33:58 +0100

    test index

--- Indexing.py
+++ Indexing.py
@@ -3,38 +3,55 @@ import json
 from typing import Callable, Dict, List, Set, Tuple
 import math
 import numpy as np
+import pprint
 from elasticsearch import Elasticsearch
 
 
-
 def index_documents(filepath: str, es: Elasticsearch, index: str) -> None:
-    """Indexes documents from file."""
-    bulk_data = []
-    cnt_passage =0
 
+    """Indexes documents from file."""
+    
+    """
+    if es.indices.exists(index):
+        es.indices.delete(index=index)
+    es.indices.create(index=index)
+    
+    cnt_passage =0
     file = open(filepath, encoding="utf8")
     read_tsv=csv.reader(file, delimiter="\t")
     print("Indexing as began...")
-    
-    
+
     for passage in read_tsv:
-        cnt_passage+=1
-        if cnt_passage==1:
-            print(passage)
-        
+        cnt_passage+=1       
         print("Indexing in progress", cnt_passage*100/8841823 , "%")
-        
-        bulk_data.append(
-            {"index": {"_index": index, "_id": passage[0:int(len(passage[0])/4)]}}
-        )
-        bulk_data.append({"body":passage[1]})
-    
-    
-    es.bulk(index=index, body=bulk_data, refresh=True)
+        print(passage)
+        es.index(index=index,doc_type="_doc",id=int(passage[0]),body=passage[1])
+
+    print("Indexing Finished.")
+    """
+    bulk_data = []
+    cnt_passage=0
+    with open(filepath, encoding="utf8") as docs:
+        read_tsv=csv.reader(docs, delimiter="\t")
+        print("Indexing as began...")
+        for passage in read_tsv:
+            cnt_passage+=1
+            print("Indexing in progress", cnt_passage*100/8841823 , "%")
+            bulk_data.append(
+                {"index": {"_index": index, "_id":int(passage[0])}}
+            )
+            bulk_data.append(passage[1])
+            
+            if cnt_passage%(1000)==0 :
+                
+                es.bulk(index=index, body=bulk_data)
+                print("reset bulk")
+                bulk_data=[]
+
     print("Indexing Finished.")
 
 def analyze_query(
-    es: Elasticsearch, query: str, index: str = "_myindex"
+    es: Elasticsearch, query: str, index: str = "myindex"
 ) -> List[str]:
     """Analyzes a query with respect to the relevant index.
 
@@ -47,21 +64,16 @@ def analyze_query(
         A list of query terms that exist in the specified field among the
         documents in the index.
     """
+    print("query",query)
     tokens = es.indices.analyze(index=index, body={"text": query})["tokens"]
+    print(tokens)
     query_terms = []
     for t in sorted(tokens, key=lambda x: x["position"]):
         # Use a boolean query to find at least one document that contains the
         # term.
-        hits = (
-            es.search(
-                index=index,
-                query={"match": {"body": t["token"]}},
-                _source=False,
-                size=1,
-            )
-            .get("hits", {})
-            .get("hits", {})
-        )
+        print(t['token'])
+        hits = es.search(index=index,query={"match": {"body": t["token"]}},_source=False,size=1,)
+        print(hits)
         doc_id = hits[0]["_id"] if len(hits) > 0 else None
         if doc_id is None:
             continue
@@ -73,10 +85,6 @@ def load_queries(filepath: str) -> Dict[str, str]:
     query strings.
 
 
-    Take as query ID the value (on the same line) after `<num> Number: `, 
-    and take as the query string the rest of the line after `<title> `. Omit
-    newline characters.
-
     Args:
         filepath: String (constructed using os.path) of the filepath to a
         file with queries.
@@ -87,22 +95,23 @@ def load_queries(filepath: str) -> Dict[str, str]:
     # TODO
     d={}
     key=""
-    with open(filepath,'r', encoding="utf-8") as file :
-        for line in file :
-            line = json.loads(line)
-            ln=line.strip().split(" ")
-            if ln[0]=="<num>" :
-                key=ln[-1]
-            elif ln[0]=="<title>" :
-                d[key]=" ".join(ln[1:])
-            line = file.readline()
-    file.close()
-    
+    with open(filepath,"r", encoding="utf-8") as file :
+        file=json.load(file)
+        for n in file:
+            key=str(n['number'])+"_"
+            for i in n['turn']:
+                d[key+str(i['number'])]=i['manual_rewritten_utterance']
+            key=""
     return d
 
 if __name__ == "__main__":
-    index_name = "_myindex"
+    index_name = "myindex3"
     es = Elasticsearch(timeout=120)
-
+    query={}
+    query_terms=[]
+    query=load_queries("2020_manual_evaluation_topics_v1.0.json")
     index_documents("collection.tsv", es,index=index_name)
+    print(es.get(index_name,id = 1))
+    query_terms=analyze_query(es, query['81_1'], index_name) 
+    print(query_terms)
 
---
+++ public_test.py
@@ -0,0 +1,10 @@
+import random
+import time
+
+import pytest
+from elasticsearch.client import Elasticsearch
+
+import Indexing as module
+
+INDEX_NAME = "_myindex"
+

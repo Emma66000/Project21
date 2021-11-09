c6ba8a10876d49e0446d5873bdae4b912c62196a
Michael <michaelmora@hotmail.fr>
2021-11-09 17:31:40 +0100

    Merging indexing in the Indexing.py file

--- Indexing.py
+++ Indexing.py
@@ -5,8 +5,12 @@ import math
 import numpy as np
 import pprint
 from elasticsearch import Elasticsearch
-
-INDEX_NAME = "myindex_3"
+from trec_car.read_data import iter_paragraphs
+from threading import Thread
+from tqdm import tqdm 
+INDEX_NAME = "index_project"
+CAR_FILE = "data/paragraphCorpus/dedup.articles-paragraphs.cbor"
+MARCO_FILE = "data/collection.tsv"
 INDEX_SETTINGS = {
     "mappings":{
         "properties":{
@@ -19,9 +23,10 @@ INDEX_SETTINGS = {
     }
 }
 
-COLLECTION_SIZE = 8841823
+MARCO_COLLECTION_SIZE = 8841823
+CAR_COLLECTION_SIZE = 29794697
 
-def index_documents(filepath: str, es: Elasticsearch, index: str) -> None:
+def index_marco_documents(filepath: str, es: Elasticsearch, index: str) -> None:
 
     """Indexes documents from the file, this function assumes that, in the file documents are separated by \t
 
@@ -38,26 +43,56 @@ def index_documents(filepath: str, es: Elasticsearch, index: str) -> None:
         read_tsv=csv.reader(docs, delimiter="\t")
 
         print("Indexing as began...")
-        for passage in read_tsv:
+        for passage in tqdm(read_tsv, desc="MARCO indexing", total=MARCO_COLLECTION_SIZE):
             cnt_passage+=1
-            if cnt_passage % int(COLLECTION_SIZE/100) == 0:
-                print("Indexing in progress", round(cnt_passage*100/COLLECTION_SIZE) , "%")
+            
             bulk_data.append(
                 {"index": {
                     "_index": index,
-                    "_id":passage[0]}
+                    "_id":f"MARCO_{passage[0]}"}
                 }
             )
             bulk_data.append({"body":passage[1]})
             
-            if cnt_passage%(100000)==0 :
-                es.bulk(index=index, doc_type="_doc", body=bulk_data, refresh=True)
-                bulk_data=[]
-            if cnt_passage == COLLECTION_SIZE:
+            if cnt_passage%(100000)==0 or cnt_passage == MARCO_COLLECTION_SIZE:
                 es.bulk(index=index, doc_type="_doc", body=bulk_data, refresh=True)
                 bulk_data=[]
 
-    print("Indexing Finished.")
+    print("Indexing Marco Finished.")
+
+def index_car_documents(filepath: str, es: Elasticsearch, index: str) -> None:
+
+    """Indexes documents from the file, this function assumes that, in the file documents are separated by \t
+
+    Params:
+        :filepath (str): path of the file to parse
+        :es (ElasticSearch): elasticsearch object to use to index documents
+        :index (str): index name to index documents
+
+    """
+    
+    bulk_data = []
+    cnt_passage=0
+    with open(filepath, mode="rb") as docs:
+        iterator = iter_paragraphs(docs)
+
+        print("Indexing as began...")
+        for p in tqdm(iterator, desc="CAR indexing", total=CAR_COLLECTION_SIZE):
+            cnt_passage+=1
+            
+            bulk_data.append(
+                {"index": {
+                    "_index": index,
+                    "_id":f"CAR_{p.para_id}"}
+                }
+            )
+            bulk_data.append({"body":p.get_text()})
+            
+            if cnt_passage%(100000)==0 or cnt_passage == CAR_COLLECTION_SIZE:
+                es.bulk(index=index, doc_type="_doc", body=bulk_data, refresh=True)
+                bulk_data=[]
+
+    print("Indexing Car Finished.")
 
 def analyze_query(
     es: Elasticsearch, query: str, index: str = "myindex"
@@ -73,16 +108,12 @@ def analyze_query(
         A list of query terms that exist in the specified field among the
         documents in the index.
     """
-    print("query",query)
     tokens = es.indices.analyze(index=index, body={"text": query})["tokens"]
-    print(tokens)
     query_terms = []
     for t in sorted(tokens, key=lambda x: x["position"]):
         # Use a boolean query to find at least one document that contains the
         # term.
-        print(t['token'])
-        hits = es.search(index=index,query={"match": {"body": t["token"]}},_source=False,size=1,)
-        print(hits)
+        hits = es.search(index=index,query={"match": {"body": t["token"]}},_source=False,size=100,)["hits"]["hits"]
         doc_id = hits[0]["_id"] if len(hits) > 0 else None
         if doc_id is None:
             continue
@@ -124,8 +155,8 @@ if __name__ == "__main__":
     query_terms=[]
     query=load_queries("data/2020_manual_evaluation_topics_v1.0.json")
     reset_index(es)
-    index_documents("data/collection.tsv", es,index=INDEX_NAME)
-    print(es.termvectors(index=INDEX_NAME, id='1'))
+    index_marco_documents(MARCO_FILE, es,INDEX_NAME)
+    index_car_documents(CAR_FILE, es, INDEX_NAME)
     query_terms=analyze_query(es, query['81_1'], INDEX_NAME) 
     print(query_terms)
 
